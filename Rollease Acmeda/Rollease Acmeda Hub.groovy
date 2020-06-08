3acaeb20dc2981ebf78a6ba9dcb24e822ff20e0f
Yoonoo <Younes@Oughla.com>
2020-06-08 12:12:20 -0400

    Update Rollease Acmeda Hub.groovy

--- Rollease Acmeda/Rollease Acmeda Hub.groovy
+++ Rollease Acmeda/Rollease Acmeda Hub.groovy
@@ -1,6 +1,11 @@
 /* 
 
-Version 2020.03.30.1
+Version 2020.06.08.1
+
+Release Notes:
+- Added Maximum Idle Time
+- Ignore invalid shade addresses
+
 
 Revision History:
 
@@ -34,10 +39,22 @@ metadata {
     input ("connectionRetryInterval", "Number",
 		title: "Connection Retry Interval", description: "Number of seconds to wait before re-attempting the connection. 0=Do Not Retry", defaultValue: "300",
                 required: false, displayDuringSetup: false )
-	
+        
+ 
+    input ("maxIdleTime", "Number",
+		title: "Maximum Idle Time", description: "Reset connection to hub if no status reports are received within this time (Seconds). 0=Disabled", defaultValue: "3600",
+                required: false, displayDuringSetup: false )
+    /*
+    input ("keepAliveInterval", "Number",
+		title: "Keep-Alive Interval", description: "Send Keep-Alive packets to hub in order to detect when connection is interrupted (Seconds). 0=Disabled", defaultValue: "300",
+                required: false, displayDuringSetup: false )
+      */
+        
     input ("debug", "bool",
 		title: "Enable debug logging", description: "", defaultValue: false,required: false, displayDuringSetup: false )
     }
+    
+
     
 }
 
@@ -46,6 +63,12 @@ def initialize () {
     telnetClose()    
     logInfo "Opening telnet connection"
     telnetConnect([termChars:[59]],hubAddress, 1487, null, null)
+    startMaxIdleTimer()
+    
+    /*
+    if (keepAliveInterval > 0) 
+        runIn(keepAliveInterval, "keepAlive");
+    */
 }
 
 
@@ -74,10 +97,11 @@ private parse(String msg)
 	logDebug("Event Received: " + msg)
     
     motorAddress = msg[1..3]
+    lastThree = msg[-3..-1]
     
 
     //Ignore Telnet Errors and Hub Events
-    if (motorAddress == "EUC" || motorAddress == "BR1" ) {
+    if (motorAddress == "EUC" || motorAddress == "BR1" || lastThree == "Enp") {
         return;
     }
     
@@ -95,6 +119,21 @@ private parse(String msg)
     
     cd.parse(msg)
 	
+    startMaxIdleTimer()
+    
+                          
+}
+
+def private startMaxIdleTimer() {
+    
+    //Reset Maximum Idle Time
+    //Y.O. 5/4/2020 No need to unschedule all timers since this affects connection retry timer. Calling runIn with the same function overwrites any existing runIns for that function
+    //unschedule() 
+    if (maxIdleTime > 0 ){
+        logDebug "Will reset connection if no events received in ${maxIdleTime} seconds"
+        runIn(maxIdleTime, "resetConnection")
+    }
+    
 }
 
 
@@ -108,11 +147,30 @@ def refresh() {
      
 }
 
+/*
+//Y.O. 5/4/2020 Hubitat does not appear to send scheduled telnet commands in the background.
+private def keepAlive() {
+    if (keepAliveInterval > 0){
+        log.info "Sending Keep-Alive"
+        sendTelnetCommand "!BR1v?"
+        runIn(keepAliveInterval, "keepAlive")
+    }
+    
+}
+*/
+
+
+private def resetConnection() {
+    log.info "Resetting Connection..."
+    initialize()
+}
+
+
 private def logDebug(message) {
     if (!debug) {
         return;   
     }
-     log.debug "${device.name} ${message}"  
+    log.debug "${device.name} ${message}"  
 }
 
 private def logInfo(message) {
