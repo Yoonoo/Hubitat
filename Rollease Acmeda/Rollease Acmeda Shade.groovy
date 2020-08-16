04bafae75a438c7be4f1c5a68008a38936fcf3a4
Kick4U2 <39640638+Kick4U2@users.noreply.github.com>
2020-08-16 16:12:08 -0400

    Cleaned code
    - Reduced whitespace
    - Added "speed" attribute

--- Rollease Acmeda/Rollease Acmeda Shade.groovy
+++ Rollease Acmeda/Rollease Acmeda Shade.groovy
@@ -1,5 +1,10 @@
 /* 
 
+Version 2020.08.16
+Release Notes:
+- Reduced whitespace
+- Added "speed" attribute
+
 Version 2020.06.08.01
 Release Notes:
 - Added "opening" and "closing" states to the windowShade attribute
@@ -14,236 +19,179 @@ Version 2020.03.30
 
 metadata {
 	definition (name: "Rollease Acmeda Shade", namespace: "arcautomate", author: "Younes Oughla", vid: "generic-shade") {
-	capability "Initialize"
-    capability "Refresh"
-    capability "Switch Level"
-    capability "Switch"
-    capability "Window Shade"
-        
-    command  "stop"
-    command  "toggle"
-        
-    attribute "open", "bool"
-    attribute "closed", "bool"
-    attribute "position", "int"
-    attribute "moving","bool"
-    attribute "voltage","int"
-        
+		capability "Initialize"
+		capability "Refresh"
+		capability "Switch Level"
+		capability "Switch"
+		capability "WindowShade"
+		command  "stop"
+		command  "toggle"
+		attribute "open", "bool"
+		attribute "closed", "bool"
+		attribute "position", "int"
+		attribute "moving","bool"
+		attribute "voltage","int"
+		attribute "speed","int"
 	}
-    
-   
-    preferences {
-	input ("motorAddress", "STRING",
-		title: "Motor Address", description: "", defaultValue: "000",
-                required: true, displayDuringSetup: true )
-        
-    input ("debug", "bool",
-		title: "Enable debug logging", description: "", defaultValue: false,required: false, displayDuringSetup: false )
+	preferences {
+		input ("motorAddress", "STRING", title: "Motor Address", description: "", defaultValue: "000", required: true, displayDuringSetup: true )
+		input ("debug", "bool", title: "Enable debug logging", description: "", defaultValue: false, required: false, displayDuringSetup: false )
 	}
-   
-    
 }
 
-
-
-def initialize(){
-	logDebug "Motor Address: ${settings?.motorAddress}"
-}
+def initialize() { logDebug "Motor Address: ${settings?.motorAddress}" }
 
 def on() {
-    open()    
+	open()
 }
 
 def off() {
-    close()    
+	close()
 }
 
 def setLevel(level) {
-    setPosition(level)
-   
+	setPosition(level)
 }
 
-def open(){
+def open() {
 	logDebug "Opening Shade"
-    sendCommand("m","000")
-    
+	sendCommand("m","000")
+	break;
 }
 
-def close(){
+def close() {
 	logDebug "Closing Shade"
-    sendCommand("m","100")
+	sendCommand("m","100")
+	break;
 }
 
 def stop() {
-  logDebug "Stopping Shade"
-  sendCommand("s","")  
+	logDebug "Stopping Shade"
+	sendCommand("s","")
+	break;
 }
 
 def toggle() {
-    logDebug "Toggling Shade"
-  
-    currentStatus = device.currentValue("windowShade")
-  
-    switch (currentStatus) {
-    
-        case "opening":
-        case "closing":
-            stop()
-            break;
-  
-        case "open":
-            close()
-            break;
-        case "closed":
-            open()
-            break;
-        case "partially open":
-            if (state.lastDirection == "closing") {
-                open()    
-            }
-            else{
-                close()
-            }    
-            break;
-        default:
-            open()
-            break;
-        
-            
-    }
-    
-    
-  
-    
+	logDebug "Toggling Shade"
+	currentStatus = device.currentValue("windowShade")
+	switch (currentStatus) {
+		case "opening":
+		case "closing":
+			stop()
+			break;
+		case "open":
+			close()
+			break;
+		case "closed":
+			open()
+			break;
+		case "partially open":
+			if (state.lastDirection == "closing") {
+				open()
+			} else {
+				close()
+			}
+			break;
+		default:
+			open()
+			break;
+	}
 }
-
 
 def setPosition(position) {
-    logDebug "Set Position: ${position}"
-    
-    positionString = 100-position
-    positionString = "000${positionString}"
-    positionString = positionString[-3..-1]
-        
-    sendCommand("m",positionString)
+	logDebug "Set Position: ${position}"
+	positionString = 100-position
+	positionString = "000${positionString}"
+	positionString = positionString[-3..-1]
+	sendCommand("m",positionString)
 }
 
-
 def sendCommand(String command, String commandData) {
-    
-    commandString = "!${motorAddress}${command}${commandData}"
-    logDebug "Send Command: ${commandString}"
-    parent.sendTelnetCommand(commandString)
+	commandString = "!${motorAddress}${command}${commandData}"
+	logDebug "Send Command: ${commandString}"
+	parent.sendTelnetCommand(commandString)
 }
 
 def parse(String msg) {
-    
-    if(msg.startsWith("!${motorAddress}r"))
-	{
-		positionStr = msg.substring(5, 8)
-
-        
-        position = 100 - Integer.parseInt(positionStr)
-
-        
-        positionUpdated(position)
-		
-   
+	switch (msg.substring(4, 5)) {
+		case "r":
+			positionStr = msg.substring(5, 8)
+			position = 100 - Integer.parseInt(positionStr)
+			positionUpdated(position)
+		case "m":
+			sendEvent(name: "moving", value: true)
+			positionStr = msg.substring(5, 8)
+			targetPosition = 100 - Integer.parseInt(positionStr)
+			currentPosition = device.currentValue("position")
+			if (targetPosition > currentPosition) {
+				sendEvent(name: "windowShade", value: "opening")
+				state.lastDirection = "opening"
+			}
+			if (targetPosition < currentPosition) {
+				sendEvent(name: "windowShade", value: "closing")
+				state.lastDirection = "closing"
+			}
+			break;
+		case "p":
+			switch (msg.substring(5, 7)) {
+				case "Vc":
+					voltageStr = msg.substring(7)
+					sendEvent(name: "voltage", value: voltageStr)
+					break;
+				case "Sc":
+					speedStr = msg.substring(7)
+					sendEvent(name: "speed", value: speedStr)
+					break;
+			}
+			break;
 	}
-    
-    if(msg.startsWith("!${motorAddress}m"))
-	{
-		sendEvent(name: "moving", value: true)
-        
-        positionStr = msg.substring(5, 8)
-        
-        targetPosition = 100 - Integer.parseInt(positionStr)
-        currentPosition = device.currentValue("position")
-     
-        
-        
-        if (targetPosition > currentPosition) {
-            sendEvent(name: "windowShade", value: "opening")
-            state.lastDirection = "opening"
-            
-        }
-        
-        if (targetPosition < currentPosition) {
-            sendEvent(name: "windowShade", value: "closing")
-            state.lastDirection = "closing"
-        }
-     
-        
-	}
-    
-    if(msg.startsWith("!${motorAddress}pVc"))
-	{
-        voltageStr = msg.substring(7)
-		sendEvent(name: "voltage", value: voltageStr)
-	}
-    
 }
-
 
 def positionUpdated(position) {
-    
-    logDebug "Position Updated: ${position}"
-        if (position == 100) {
-            sendEvent(name: "open", value: true)
-            sendEvent(name: "closed", value: false)
-            sendEvent(name: "windowShade", value: "open")
-        }
-        
-        if (position == 0) {
-            sendEvent(name: "closed", value: true)  
-            sendEvent(name: "open", value: false)  
-            sendEvent(name: "windowShade", value: "closed")
-        }
-        
-        if (position !=0 && position !=100) {
-            sendEvent(name: "closed", value: false)   
-            sendEvent(name: "open", value: false)
-            sendEvent(name: "windowShade", value: "partially open")
-        }
-        
-        
-        sendEvent(name: "position", value: position)
-        sendEvent(name: "moving", value: false)
+	logDebug "Position Updated: ${position}"
+	if (position == 100) {
+		sendEvent(name: "open", value: true)
+		sendEvent(name: "closed", value: false)
+		sendEvent(name: "windowShade", value: "open")
+	} else if (position == 0) {
+		sendEvent(name: "closed", value: true)
+		sendEvent(name: "open", value: false)
+		sendEvent(name: "windowShade", value: "closed")
+	} else {
+		sendEvent(name: "closed", value: false)
+		sendEvent(name: "open", value: false)
+		sendEvent(name: "windowShade", value: "partially open")
+	}
+	sendEvent(name: "position", value: position)
+	sendEvent(name: "moving", value: false)
 }
-
-
 
 def requestStatus() {
-    sendCommand("r","?")
+	sendCommand("r","?")
 }
 
-
 def refresh() {
-   logDebug "Refreshing"
-   requestStatus()
-    
+	logDebug "Refreshing"
+	requestStatus()
 }
 
 private def logDebug(message) {
-    if (!debug) {
-        return;   
-    }
-     log.debug "${device.name} ${message}"  
+	if (!debug) { return; }
+	log.debug "${device.name} ${message}"  
 }
 
 private def logInfo(message) {
-     log.info "${device.name} ${message}"  
+	log.info "${device.name} ${message}"
 }
 
 private def logWarning(message) {
-     log.warn "${device.name} ${message}"  
+	log.warn "${device.name} ${message}"
 }
 
-
-def installed(){
+def installed() {
 	initialize()
 }
 
-def updated(){
+def updated() {
 	initialize()
 }
